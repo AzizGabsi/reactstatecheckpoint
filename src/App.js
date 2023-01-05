7b4dd6c3b9a198afcf95437d3efb0e2adb405f97
AzizGabsi <aziz.gabsi@outlook.com>
2023-01-05 01:06:53 +0100

    adding checkpoint files (unfinished)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+import Photo from "./photo.jpg"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+   state = {
+    Person : {fullName:"Aziz Gabsi", bio: "22 year old man", imgSrc:Photo, profession: "Full-Stack Web Developer"},
+    shows: true
+  }
+
+
+  togglePerson = () => {
+    this.setState(previousState => ({
+      shows: !previousState.shows
+    }))
+  }
+
+  render() {
+    return (
+      <div className='App'> {this.state.shows && (
+      <>
+        <img src={this.state.Person.imgSrc}/>
+        <h3>{this.state.Person.fullName}</h3>
+        <h3>{this.state.Person.bio}</h3>
+        <h3>{this.state.Person.profession}</h3>
+      </>
+      )}
+        <button className='button' onClick={this.togglePerson}>Click Here</button>
+      </div>
+    )
+  };
 }
+
+
 
 export default App;
